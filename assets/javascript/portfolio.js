60dc0cac4aeba32825dc28043dd523e0681a2f85
Fadodo <fadydrive96@gmail.com>
2018-10-23 11:01:16 -0700

    updated

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,12 @@
+// $(".overlay").on("click", function on (){
+//     $("#overlay").style.display = "block"
+// })
+
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
